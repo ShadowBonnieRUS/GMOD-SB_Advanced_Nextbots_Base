6fe825973726446cbbf16a62adc3c0edafd5f217
Shadow Bonnie (RUS) <49488893+ShadowBonnieRUS@users.noreply.github.com>
2021-02-15 21:20:36 +0300

    Added crosshair support

--- sb_advanced_nextbots/lua/entities/sb_advanced_nextbot_base/cl_playercontrol.lua
+++ sb_advanced_nextbots/lua/entities/sb_advanced_nextbot_base/cl_playercontrol.lua
@@ -6,10 +6,12 @@
 	Arg2: number | y | Y HUD coordinate.
 	Arg3: number | w | Width of health bar.
 	Arg4: number | h | Height of health bar.
+	Arg5: number | chx | X HUD coordinate of crosshair.
+	Arg6: number | chy | Y HUD coordinate of crosshair.
 	Ret1: bool | Return true to prevent drawing default HUD.
 --]]------------------------------------
-function ENT:ModifyPlayerControlHUD(x,y,w,h)
-	return self:RunTask("ModifyPlayerControlHUD",x,y,w,h)
+function ENT:ModifyPlayerControlHUD(x,y,w,h,chx,chy)
+	return self:RunTask("ModifyPlayerControlHUD",x,y,w,h,chx,chy)
 end
 
 local function GetControlledBot()
@@ -31,7 +33,23 @@ hook.Add("HUDPaint","SBAdvancedNextBotControl",function()
 	local p = bot:WorldSpaceCenter():ToScreen()
 	local w,h = 70,15
 	
-	if bot:ModifyPlayerControlHUD(p.x,p.y,w,h) then return end
+	local crosshair = util.TraceLine({start = bot:GetShootPos(),endpos = bot:GetShootPos()+bot:GetEyeAngles():Forward()*56756,mask = MASK_SHOT,filter = function(ent)
+		if ent==bot or ent:GetParent()==bot then return false end
+		if (bot:GetCustomCollisionCheck() or ent:GetCustomCollisionCheck()) and !gamemode.Call("ShouldCollide",bot,ent) then return false end
+		
+		return true
+	end})
+	
+	local chp = crosshair.HitPos:ToScreen()
+	
+	if bot:ModifyPlayerControlHUD(p.x,p.y,w,h,chp.x,chp.y) then return end
+	
+	surface.SetDrawColor(255,213,86)
+	surface.DrawRect(chp.x,chp.y,1,1)
+	surface.DrawRect(chp.x-10,chp.y,1,1)
+	surface.DrawRect(chp.x+10,chp.y,1,1)
+	surface.DrawRect(chp.x,chp.y-8,1,1)
+	surface.DrawRect(chp.x,chp.y+8,1,1)
 	
 	local hp = math.max(0,bot:Health())
 	local maxhp = bot:GetMaxHealth()
