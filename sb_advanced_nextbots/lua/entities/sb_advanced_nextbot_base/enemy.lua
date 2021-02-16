0cb84635893244e16968039ac6446bac25cab7ed
Jellyton <bearywagon@gmail.com>
2021-02-16 13:01:23 -0800

    Update enemy.lua

--- sb_advanced_nextbots/lua/entities/sb_advanced_nextbot_base/enemy.lua
+++ sb_advanced_nextbots/lua/entities/sb_advanced_nextbot_base/enemy.lua
@@ -224,8 +224,8 @@ end
 	Ret1: bool | Should be enemy or not
 --]]------------------------------------
 function ENT:ShouldBeEnemy(ent)
+	if ent:IsFlagSet(FL_NOTARGET) then return false end
 	if ent:IsPlayer() and GetConVar("ai_ignoreplayers"):GetBool() then return false end
-	if ent:IsPlayer() and ent:IsFlagSet(FL_NOTARGET) then return false end
 	if self:GetRelationship(ent)!=D_HT then return false end
 	if !ent.SBAdvancedNextBot and ent:IsNPC() and (ent:GetNPCState()==NPC_STATE_DEAD or ent:GetClass()=="npc_barnacle" and ent:GetInternalVariable("m_takedamage")==0) then return false end
 	if (ent.SBAdvancedNextBot or !ent:IsNPC()) and ent:Health()<=0 then return false end
